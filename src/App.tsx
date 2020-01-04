a492f9e988fbcb5ef643d4ac08f683f42d6c3cce
Dan Alvares <dm.alvares@gmail.com>
2020-01-04 14:28:45 +0200

    Update testing library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export const App = () => (<div>
-  <div>✔ React</div>
-  <div>✔ Typescript</div>
-  <div>✔ Prettier</div>
-  <div>✔ Parcel</div>
-  <div>✔ Jest</div>
-</div>
+export const App = () => (
+  <div>
+    <div>✔ React</div>
+    <div>✔ Typescript</div>
+    <div>✔ Prettier</div>
+    <div>✔ Parcel</div>
+    <div>✔ Jest</div>
+  </div>
 )
