4ed0e9019d7b0867b52b95e116ba3065e8c2bc9b
Dan Alvares <dm.alvares@gmail.com>
2020-10-05 09:23:58 +0100

    Add image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import FirebaseDemo from './FirebaseDemo'
-import tech from './assets/images/image.jpg';
+import tech from './assets/images/image.jpg'
 
 export const App = () => (
   <div className="text-center">
     <picture>
-      <img src={tech} alt="React TypeScript Parcel" style={{ maxWidth: '100%' }} />
+      <img
+        src={tech}
+        alt="React TypeScript Parcel"
+        style={{ maxWidth: '100%' }}
+      />
     </picture>
     <FirebaseDemo />
   </div>
